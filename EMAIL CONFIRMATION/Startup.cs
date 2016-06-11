32136aa6fa9a46aa6d88e3d383a04f906cd8a717
KIEROWNIK <KIEROWNIK@DELL-CENTRUM>
2016-06-11 20:37:59 +0200

    Add project files.

---
+++ EMAIL CONFIRMATION/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EMAIL_CONFIRMATION.Startup))]
+namespace EMAIL_CONFIRMATION
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
